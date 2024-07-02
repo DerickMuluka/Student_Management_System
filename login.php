264a46c7214438ced0619d5b5da3419622d50f53
Derick Muluka <mulukaderick@gmail.com>
2024-07-02 17:57:54 +0300

    Add basic login page UI

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>LoginPage</title>
+</head>
+<body>
+    <div class="loginform">
+        <div class="inputgroup">
+            <input type="text" id="txtUsername" required>
+            <label for="txtusername" id="lblPassword">USER NAME</label>
+        </div>
+        <div class="inputgroup topmarginlarge">
+            <input type="password" id="txtPassword" required>
+            <label for="txtpassword" id="lblPassword">PASSWORD</label>
+        </div>
+        <div class="divcallforaction topmarginlarge">
+            <button class="btnlogin">LOGIN</button>
+        </div>
+    </div>
+</body>
+</html>
