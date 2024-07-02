393dc1dfde6d6d5ff5b8347e93f8a3f289b6b43c
Derick Muluka <mulukaderick@gmail.com>
2024-07-02 11:34:25 +0300

    Insert initial data into student, faculty, session, and course tables

--- database/createtables.php
+++ database/createtables.php
@@ -116,4 +116,83 @@ try
 } catch (PDOException $o) {
     echo("<br>attendance_details not created ");
 }
+
+$c = "insert into student_details
+(id,roll_no,name)
+values
+(1,'CSB21001','DERYK CHEED'),
+(2,'CSB21002','ESSY JUMA'),
+(3,'CSB21003','EVANS RUPIN'),
+(4,'CSB21004','DEBS RICHARD'),
+(5,'CSB21005','SAMMY TONNY'),
+(6,'CSB21006','LINET SHIKAI'),
+(7,'CSB21007','EZRA CHILOBA'),
+(8,'CSB21008','TESSY DIVA'),
+(9,'CSB21009','TRISHA BASHIR'),
+(10,'CSB21010','SAID MARO'),
+(11,'CSB21011','PETER TUSH'),
+(12,'CSB21012','FATUMA GEDI'),
+(13,'CSM21001','JOAN SHEBE'),
+(14,'CSM21002','SASHA JULLY'),
+(15,'CSM21003','REMMY TASH'),
+(16,'CSM21004','OSCAR MATHA'),
+(17,'CSM21005','DIVOCK ORIGI'),
+(18,'CSM21006','HIJAH KHALID'),
+(19,'CSM21007','SARAH TOHA'),
+(20,'CSM21008','BUTTON SEIF'),
+(21,'CSM21009','ERIC BILLY'),
+(22,'CSM21010','OMAR SWALE'),
+(23,'CSM21011','ABDI GORO'),
+(24,'CSM21012','NATHAN JOB')";
+
+$s=$dbo->conn->prepare($c);
+try {
+    $s->execute();
+} catch (PDOException $o) {
+    echo("<br>duplicate entry");
+}
+
+$c = "insert into faculty_details
+(id, user_name, name, password)
+values
+(1, 'rcb', 'RCB', '123'),
+(2, 'dr', 'Deryk Raun', '123'),
+(3, 'lh', 'Linda Hendriks', '123'),
+(4, 'mj', 'Matano Juma', '123'),
+(5, 'ca', 'Chris Abdi', '123'),
+(6, 'pl', 'Priscar Latiffah', '123')";
+$s=$dbo->conn->prepare($c);
+try {
+    $s->execute();
+} catch (PDOException $o) {
+    echo("<br>duplicate entry");
+}
+
+$c = "insert into session_details
+(id,year,term) 
+values
+(1,2024,'SPRING SEMESTER'),
+(2,2024,'AUTUMN SEMESTER')";
+$s=$dbo->conn->prepare($c);
+try {
+    $s->execute();
+} catch (PDOException $o) {
+    echo("<br>duplicate entry");
+}
+
+$c = "insert into course_details
+(id,title,code,credit)
+values
+(1,'Database Management System lab','CO321',2),
+(2,'Pattern Recognition','CO215',3),
+(3,'Data Mining & Data Warehousing','CS112',4),
+(4,'Artificial Intelligence','CS670',4),
+(5,'Theory Of Computation','CO432',3),
+(6,'Demystifying Networking','CS673',1)";
+$s=$dbo->conn->prepare($c);
+try {
+    $s->execute();
+} catch (PDOException $o) {
+    echo("<br>duplicate entry");
+}
 ?>
